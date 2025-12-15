1b261bd92ac3574f92cc2260399d366b6cb35823
valtermro <valtermro@outlook.com>
2025-12-15 13:31:16 -0300

    RGB indicator

--- keyboards/keebio/iris_ce/rev1/keymaps/valtermro/rgb.c
+++ keyboards/keebio/iris_ce/rev1/keymaps/valtermro/rgb.c
@@ -4,9 +4,8 @@ void rgb_init(void) {
 }
 
 void rgb_update(uint8_t led_min, uint8_t led_max) {
-#if false
+#if true
     uint8_t mods = get_mods() | get_oneshot_mods();
-    bool has_caps_lock = host_keyboard_led_state().caps_lock;
 #   define HAS_MOD(m) (mods & MOD_BIT(m))
 
     for (uint8_t row = 0; row < MATRIX_ROWS; ++row) {
@@ -17,55 +16,54 @@ void rgb_update(uint8_t led_min, uint8_t led_max) {
                 continue;
             }
 
-#           define HRM_LGUI_INDICATOR  2
-#           define HRM_LALT_INDICATOR  3
-#           define HRM_RALT_INDICATOR  3
-#           define HRM_LSFT_INDICATOR  5
-#           define HRM_LCTL_INDICATOR  6
+#           define HRM_LGUI_INDICATOR  0 // 2
+#           define HRM_LALT_INDICATOR  0 // 3
+#           define HRM_RALT_INDICATOR  0 // 3
+#           define HRM_LSFT_INDICATOR  0 // 5
+#           define HRM_LCTL_INDICATOR  0 // 6
 #           define CAPS_INDICATOR      0
-#           define DM_REC_INDICATOR    33
+#           define DM_REC_INDICATOR    0 // 33
 
             hsv_t hsv = { .h = 0, .s = 255, .v = 75 };
-#           define HUE_INDICATOR  128 // Blue
-#           define HUE_INDICATOR2 191 // Purple
-#           define HUE_ACTIVE_1   43  // Yellow
-#           define HUE_ACTIVE_2   85  // Green
-#           define HUE_ACTIVE_3   1   // Red
-#           define HUE_ACTIVE_4   191 // Purple
+#           define HUE_0 128 // Blue
+#           define HUE_1 85  // Green
+#           define HUE_2 43  // Yellow
+#           define HUE_3 1   // Red
+#           define HUE_4 191 // Purple
+
+            if (index == DM_REC_INDICATOR) 
+            {
+                if (global_state.is_recording_macro_1 || global_state.is_recording_macro_2)
+                    hsv.h = HUE_3;
+            }
+
+            if (index == HRM_RALT_INDICATOR && HAS_MOD(KC_RALT))
+            {
+                hsv.h = HUE_4;
+            }
 
             if ((index == HRM_LALT_INDICATOR && HAS_MOD(KC_LALT)) ||
                 (index == HRM_LGUI_INDICATOR && HAS_MOD(KC_LGUI)) ||
                 (index == HRM_LSFT_INDICATOR && HAS_MOD(KC_LSFT)))
             {
-                hsv.h = HUE_INDICATOR;
-            }
-            else if ((index == HRM_RALT_INDICATOR && HAS_MOD(KC_RALT)))
-            {
-                hsv.h = HUE_INDICATOR2;
-            }
-            else if (index == HRM_LCTL_INDICATOR) {
-                switch (global_state.prefixed_ctl_key) {
-                    case KC_K:
-                        hsv.h = HUE_ACTIVE_2;
-                        break;
-                    default:
-                        if (HAS_MOD(KC_LCTL))
-                            hsv.h = HUE_INDICATOR;
-                        break;
-                }
-            }
-            else if (index == CAPS_INDICATOR) {
-                if (has_caps_lock)
-                    hsv.h = HUE_ACTIVE_1;
-            }
-            else if (index == DM_REC_INDICATOR) {
-                if (global_state.is_recording_macro_2)
-                    hsv.h = HUE_ACTIVE_1;
-                else if (global_state.is_recording_macro_1)
-                    hsv.h = HUE_INDICATOR;
+                hsv.h = HUE_0;
             }
 
-            if (hsv.h != 0) {
+            if (index == HRM_LCTL_INDICATOR) 
+            {
+                if (global_state.prefixed_ctl_key)
+                    hsv.h = HUE_1;
+                else if (HAS_MOD(KC_LCTL))
+                    hsv.h = HUE_0;
+            }
+
+            // if (index == CAPS_INDICATOR && host_keyboard_led_state().caps_lock)
+            // {
+            //     hsv.h = HUE_;
+            // }
+
+            if (hsv.h != 0) 
+            {
                 rgb_t rgb = hsv_to_rgb(hsv);
                 rgb_matrix_set_color(index, rgb.r, rgb.g, rgb.b);
             }
